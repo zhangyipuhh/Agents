609a5b532a949f03871d7aa0af75fedd93ace404
zhangyipu <542995981@qq.com>
2026-01-08 18:57:55 +0800

    文件修改 文件修改为空白

--- app/agents/config/__init__.py
+++ app/agents/config/__init__.py
@@ -1,5 +1,2 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
-from .config import Config, get_config, config
-
-__all__ = ['Config', 'get_config', 'config']
--- app/agents/llmcalls/__init__.py
+++ app/agents/llmcalls/__init__.py
@@ -1,7 +1,3 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
-from .model_factory import ModelFactory
-from .deepseek import create_model as create_deepseek_model
-from .ollama import create_model as create_ollama_model
 
-__all__ = ['ModelFactory', 'create_deepseek_model', 'create_ollama_model']
