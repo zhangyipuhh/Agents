74fda066bedfd89245632a0dc7a8871930c6a71c
zhangyipu <542995981@qq.com>
2026-01-09 10:27:35 +0800

    文件修改 主提示词修改

--- app/agents/config/config.py
+++ app/agents/config/config.py
@@ -13,3 +13,9 @@ LLM_CONFIG = {
     "base_url": os.getenv("model_api_base"),
     "model_type":os.getenv("model_type")
 }
+
+PROMPT_TEMPLATE = {
+    #主提示词
+    "main":"你是自然资源业务任务，通过合适的工具回答问题，不能凭空回答。"
+}
+
