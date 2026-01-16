125aa0783b9841092617a6bfdd947ee2ab1ad6d3
zhangyipu <542995981@qq.com>
2026-01-16 10:43:48 +0800

    添加文件 在 合同审核智能体中添加配置文件和init文件

---
+++ app/agents/subgraphs/audit_contract_clause/config/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-  
+import os
+from dotenv import load_dotenv
+from enum import Enum
+
+# 从.env文件中加载环境变量
+# 这会将.env文件中的变量加载到os.environ中，供后续代码使用
+load_dotenv()
