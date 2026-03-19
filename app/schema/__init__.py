90504411da47d1d2d19c58a00c4710e7be7259c2
zhangyipu <542995981@qq.com>
2026-03-19 15:56:36 +0800

    文件修改 req修改

---
+++ app/schema/__init__.py
@@ -0,0 +1,9 @@
+"""
+app.schema 包
+
+该包包含数据结构的Schema定义，用于标准化请求和响应的数据结构。
+"""
+
+from app.schema.req import ToolChunkResponse
+
+__all__ = ["ToolChunkResponse"]
---
+++ app/schema/req/__init__.py
@@ -0,0 +1,9 @@
+"""
+app.schema.req 包
+
+该包包含请求相关的Schema定义，用于标准化各种请求和响应的数据结构。
+"""
+
+from app.schema.req.ToolChunkResponse import ToolChunkResponse
+
+__all__ = ["ToolChunkResponse"]
